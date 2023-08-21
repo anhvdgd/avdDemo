c35c7fc907baf953cba1f95b338b34eabf105000
unknown <anhvd@rikkeisoft.com>
2023-08-21 18:06:55 +0700

    redux first touch

---
+++ src/Home/fakeData.js
@@ -0,0 +1,56 @@
+export const fakeData = [
+    {
+        name: "Tee",
+        size: "XL",
+        color: "Blue",
+        price: 50,
+    },
+    {
+        name: "Socks",
+        size: "L",
+        color: "Blue",
+        price: 50,
+    },
+    {
+        name: "Shorts",
+        size: "XL",
+        color: "Blue",
+        price: 350,
+    },
+    {
+        name: "Vest",
+        size: "M",
+        color: "Blue",
+        price: 50,
+    },
+    {
+        name: "Polos",
+        size: "S",
+        color: "Blue",
+        price: 150,
+    },
+    {
+        name: "Jacket",
+        size: "XS",
+        color: "Blue",
+        price: 550,
+    },
+    {
+        name: "Shirts",
+        size: "XXL",
+        color: "Blue",
+        price: 50,
+    },
+    {
+        name: "Jeans",
+        size: "XXS",
+        color: "Blue",
+        price: 50,
+    },
+    {
+        name: "Pants",
+        size: "XL",
+        color: "Blue",
+        price: 50,
+    },
+]
---
+++ src/Playgound/Playground.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from 'react';
+
+
+function Playground(){
+    return(
+        <div>
+            <h1>Playground</h1>
+        </div>
+    )
+}
+
+export default Playground;
