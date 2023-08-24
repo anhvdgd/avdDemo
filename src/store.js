131e3d3e4d9149de664388f6eefea6bcf02124a9
unknown <anhvd@rikkeisoft.com>
2023-08-24 10:09:22 +0700

    delete all

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./actions/counterSlice";
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
-})
